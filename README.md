cc16e3cf830e656cdbaadd271ea8b720061e9b29
Ulysses Carlos <ulycarlos@gmail.com>
2019-10-19 22:55:14 -0400

    Program can be run with command-line arguments.

--- README.md
+++ README.md
@@ -4,7 +4,15 @@ with Single Precision (a "float") or Double Precision (a "double")
 and separates it into its Significand, Sign, Exponent and Mantissa 
 components.
 
-##Usage
+## Usage
 ```bash
 ./Float_to_IEEE
 ```
+You may also run the program with command line arguments:
+```bash
+./Float_to_IEEE [Precision] [Number]
+```
+Where [Precision] can be the following values:
+
+* float
+* double
--- Test_Float.c
+++ Test_Float.c
@@ -5,10 +5,10 @@
 //--------------------------------------------------------------------------------------------------
 
 #include "Float_to_IEE.h"
+const int max_length = 100;
 
 void test_edit(void){
     struct float_number *fn = calloc(1, sizeof(struct float_number));
-    int max_length = 10;
     char string[max_length];
     strncpy(string, "FLOAT", sizeof("FLOAT"));
 
@@ -36,8 +36,59 @@ void compute(void){
 
 }
 
-int main(void){
-    compute();
+void compute_with_value(char *string, int length, double value){
+    struct float_number *fn = calloc(1, sizeof(struct float_number));
+
+    char temp[max_length];
+    strncpy(temp, string, strlen(string));
+    Edit_Floating_Number(fn, temp, value);
+    Print_Float_Number(fn);
+
+    Additional_Info();
+    free(fn);
+
+}
+
+void command_instructions(void){
+    puts("Usage: ./Float_to_IEEE [PRECISION] [Number]");
+    puts("[PRECISION] may be one of the following options:");
+    puts("float -- Single Precision");
+    puts("double -- Double Precision");
+
+
+}
+
+int main(int argc, char *argv[]){
+    // User passed only ./Float_to_IEEE as argument
+    if (argc == 1)
+        compute();
+    else{
+        // Usage: ./Float_to_IEEE, float/double [Number]
+        // First make sure that argc == 3, if not exit.
+        if (argc != 3){
+            command_instructions();
+            exit(EXIT_FAILURE);
+        }
+        char *precision = calloc(max_length, sizeof(char));
+        int precision_len  = sizeof(argv[1]);
+        strncpy(precision, argv[1], precision_len);
+
+        bool check = (!strncmp(precision, "double", precision_len)
+                    || !strncmp(precision, "float", precision_len));
+
+        if (!check){
+            printf("%100s is an invalid precision option.", precision);
+            exit(EXIT_FAILURE);
+        }
+
+        char *temp;
+        double value = strtod(argv[2], &temp);
+
+        compute_with_value(precision, precision_len, value);
+
+
+    }
+
     return 0;
 }
 
